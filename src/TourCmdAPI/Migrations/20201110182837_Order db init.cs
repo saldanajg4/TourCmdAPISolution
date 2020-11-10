c77a21af1d4522a3e8ebf5d94928137b3b714bc3
saldanajg4 <saldanajg4@hotmail.com>
2020-11-10 14:01:28 -0600

    Redeploying api app

---
+++ src/TourCmdAPI/Migrations/20201110182837_Order db init.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TourCmdAPI.Migrations
+{
+    public partial class Orderdbinit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CardOwnerName",
+                table: "PaymentDetails",
+                maxLength: 70,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CardOwnerName",
+                table: "PaymentDetails",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 70);
+        }
+    }
+}
